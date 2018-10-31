5428f779696ad6b4b77e9f906c872129679aa232
mitko <kostov.email@gmail.com>
2018-10-31 23:25:18 +0200

    go go

---
+++ Resolving.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InjectDemo
+{
+    public abstract class Resolving<T>
+    {
+        private static Func<T> activator;
+
+        private static T constant;
+
+        protected T implementation;
+
+        public Resolving()
+        {
+            implementation = activator();
+        }
+
+        public static T Constant()
+        {
+            return constant;
+        }
+
+        public static void Is(Func<T> activator)
+        {
+            Resolving<T>.activator = activator;
+            constant = activator();
+        }
+    }
+}
